2e0b3c270cfa8f40fa050d2458f89f582026b073
Wojciech <wojtix144@gmail.com>
2023-04-27 19:00:21 +0200

    Play silent audio in loop

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿ManualResetEvent _quitEvent = new ManualResetEvent(false);
+Console.CancelKeyPress += (sender, eArgs) => {
+	_quitEvent.Set();
+	eArgs.Cancel = true;
+};
+
+System.Media.SoundPlayer player = new System.Media.SoundPlayer(@".\Silence.wav");
+player.PlayLooping();
+
+_quitEvent.WaitOne();
